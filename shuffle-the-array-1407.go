d1588a3e3b1518b29810c774efd998074e6b1718
Koby Conrad <52094714+kobyconrad@users.noreply.github.com>
2020-09-03 12:45:17 -0700

    shuffled the array

--- shuffle-the-array-1407.go
+++ shuffle-the-array-1407.go
@@ -3,25 +3,36 @@ package main
 import "fmt"
 
 func shuffle(nums []int, n int) []int {
-	fmt.Println(nums)
-	fmt.Println(n)
+
+	startArr := nums[:n]
+	endArr := nums[n:]
+	answerArr := []int{}
+
+	// fmt.Println(startArr)
+	// fmt.Println(endArr)
+
+	for i := 0; i < n; i++ {
+		answerArr = append(answerArr, startArr[i])
+		answerArr = append(answerArr, endArr[i])
+	}
+
+	fmt.Println(answerArr)
 
 	myIntReturn := []int{2}
-
 	return myIntReturn
 }
 
 func main() {
 
-	myArr := []int{1, 2, 3}
-	// myInt := 3
-	// shuffle(myArr, myInt)
+	myArr := []int{2, 5, 1, 3, 4, 7}
+	myInt := 3
+	shuffle(myArr, myInt)
 
-	startArr := myArr[:2]
-	endArr := myArr[2:]
+	// startArr := myArr[:2]
+	// endArr := myArr[2:]
 
-	fmt.Println(startArr)
-	fmt.Println(endArr)
+	// fmt.Println(startArr)
+	// fmt.Println(endArr)
 
 	// for _, item := range myArr {
 	// 	fmt.Println(item)
